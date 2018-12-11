8039325fafedaccac6d88eedd995a57566113c38
hu970804 <812510003@qq.com>
2018-12-11 14:48:39 +0800

    add life and slide vue

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,11 @@
   <div class="page-index">
     <el-row>
       <el-col :span="5">
-        <emenu></emenu>
+        <emenu/>
       </el-col>
-      <el-col :span="19">2</el-col>
+      <el-col :span="19">
+        <life/>
+      </el-col>
     </el-row>
     <el-row :span="24">3</el-row>
   </div>
@@ -12,10 +14,12 @@
 
 <script>
 import Emenu from '@/components/index/menu.vue'
+import Life from '@/components/index/life.vue'
 
 export default {
   components: {
-    Emenu
+    Emenu,
+    Life
   }
 }
 </script>
