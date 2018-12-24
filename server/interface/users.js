66bf93700cb230f37d4d95e2ef6a8504a942f321
blackhu <812510003@qq.com>
2018-12-24 21:52:40 +0800

    add signin exit getUser interface

--- server/interface/users.js
+++ server/interface/users.js
@@ -84,3 +84,102 @@ router.post('/signup', async (ctx) => {
     }
   }
 })
+
+router.post('/signin', async (ctx, next) => {
+  return Passport.authenticate('local', function(err, user, info, status){
+    if(err) {
+      ctx.body = {
+        code: -1,
+        msg: err
+      }
+    } else {
+      if(user) {
+        ctx.body = {
+          code: 0,
+          msg: '登陆成功',
+          user
+        }
+        return ctx.login(user)
+      } else {
+        ctx.body = {
+          code: 1,
+          msg: info
+        }
+      }
+    }
+  })(ctx, next)
+})
+
+router.post('/verify', async (ctx, next) => {
+  let user = ctx.request.body.username
+  const saveExpire = await Store.hget(`nodemail: ${username}`, 'expire')
+  if(saveExpire && new Date().getTime() - saveExpire < 0) {
+    ctx.body = {
+      code: -1,
+      msg: '验证请求过于频繁！'
+    }
+    return false
+  }
+  let transporter = nodeMailer.createTransport({
+    host: Email.host,
+    prot: 587,
+    secure: false,
+    auth: {
+      user: Email.smtp.user,
+      pass: Email.smtp.pass
+    }
+  })
+  let ko = {
+    code: Email.smtp.code(),
+    expire: Email.smtp.expire(),
+    email: ctx.request.body.email,
+    user: ctx.request.body.username
+  }
+  let mailOptions = {
+    from: `"认证邮件"<${Email.smtp.user}>`,
+    to: ko.email,
+    subject: `您申请的注册码`,
+    html: `您申请的验证码是${ko.code}`
+  }
+  await transporter.sendMail(mailOptions, (error, info) => {
+    if(error) {
+      return console.log('err')
+    } else {
+      Store.hmset(`nodemail:${ko.user}`, 'code', ko.code, 'expire', ko.expire, 'email', ko.email)
+    }
+  })
+  ctx.body = {
+    code: 0,
+    msg: '验证码已发送，验证码有效期为一分钟！'
+  }
+})
+
+router.get('/exit', async (ctx, next) => {
+  await ctx.logout()
+  if(!ctx.isAuthenticated()) {
+    ctx.body = {
+      code: 0
+    }
+  } else {
+    ctx.body = {
+      code: -1
+    }
+  }
+})
+
+router.get('/getUser', async(ctx) => {
+  if(ctx.isAuthenticated()) {
+    const {username, email} = ctx.session.passport.user
+    ctx.body = {
+      user: username,
+      email
+    }
+  } else {
+    ctx.body = {
+      user: '',
+      email: ''
+    }
+  }
+})
+
+export default router
