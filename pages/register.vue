958e8a44911cd02ab179e3c6cce76aab68cdae69
blackhu <812510003@qq.com>
2018-12-27 23:20:38 +0800

    完成登录注册

--- pages/register.vue
+++ pages/register.vue
@@ -12,30 +12,59 @@
       </header>
     </article>
     <section>
-      <el-form ref="ruleForm" :model="ruleForm" :rules="rules" label-width="100px" class="demo-ruleForm">
-        <el-form-item label="昵称" prop="name">
+      <el-form
+        ref="ruleForm"
+        :model="ruleForm"
+        :rules="rules"
+        label-width="100px"
+        class="demo-ruleForm">
+        <el-form-item
+          label="昵称"
+          prop="name">
           <el-input v-model="ruleForm.name" />
         </el-form-item>
-        <el-form-item label="邮箱" prop="email">
+        <el-form-item
+          label="邮箱"
+          prop="email">
           <el-input v-model="ruleForm.email" />
-          <el-button size="mini" round @click="sendMsg">发送验证码</el-button>
+          <el-button
+            size="mini"
+            round
+            @click="sendMsg">发送验证码</el-button>
           <span class="status">{{ statusMsg }}</span>
         </el-form-item>
-        <el-form-item label="验证码" prop="code">
-          <el-input v-model="ruleForm.code" maxlength="4" />
+        <el-form-item
+          label="验证码"
+          prop="code">
+          <el-input
+            v-model="ruleForm.code"
+            maxlength="4" />
         </el-form-item>
-        <el-form-item label="密码" prop="pwd">
-          <el-input v-model="ruleForm.pwd" type="password" />
+        <el-form-item
+          label="密码"
+          prop="pwd">
+          <el-input
+            v-model="ruleForm.pwd"
+            type="password" />
         </el-form-item>
-        <el-form-item label="确认密码" prop="cpwd">
-          <el-input v-model="ruleForm.cpwd" type="password" />
+        <el-form-item
+          label="确认密码"
+          prop="cpwd">
+          <el-input
+            v-model="ruleForm.cpwd"
+            type="password" />
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="register">同意以下协议并注册</el-button>
+          <el-button
+            type="primary"
+            @click="register">同意以下协议并注册</el-button>
           <div class="error">{{ error }}</div>
         </el-form-item>
         <el-form-item>
-          <a class="f1" href="http://www.meituan.com/about/terms" target="_blank">《美团网用户协议》</a>
+          <a
+            class="f1"
+            href="http://www.meituan.com/about/terms"
+            target="_blank">《美团网用户协议》</a>
         </el-form-item>
       </el-form>
     </section>
@@ -43,6 +72,7 @@
 </template>
 
 <script>
+  import CryptoJS from 'crypto-js'
   export default {
     layout: "blank",
     data() {
@@ -71,13 +101,11 @@
           }],
           pwd: [{
             required: true,
-            type: 'string',
-            message: '请输入密码',
+            message: '创建密码',
             trigger: 'blur'
           }],
           cpwd: [{
             required: true,
-            type: 'string',
             message: '确认密码',
             trigger: 'blur'
           }, {
@@ -87,10 +115,10 @@
               } else if (value !== this.ruleForm.pwd) {
                 callback(new Error('两次输入密码不一致'))
               } else {
-                callback
+                callback()
               }
             },
-            trigger: "blur"
+            trigger: 'blur'
           }]
         }
       }
@@ -134,7 +162,32 @@
         }
       },
       register() {
-
+        let self = this
+        
+        this.$refs['ruleForm'].validate( (valid) => {
+          if(valid) {
+          console.log(1111);
+            self.$axios.post('/users/signup', {
+              username: window.encodeURIComponent(self.ruleForm.name),
+              password: CryptoJS.MD5(self.ruleForm.pwd).toString(),
+              email: self.ruleForm.email,
+              code: self.ruleForm.code
+            }).then( ({status, data}) => {
+              if(status === 200) {
+                if(data && data.code === 0) {
+                  location.href = '/login'
+                } else {
+                  self.error = data.msg
+                }
+              } else {
+                self.error = `服务器出错，错误码：${status}`
+              }
+              setTimeout(function() {
+                self.error = ''
+              }, 1500)
+            })
+          }
+        })
       }
     }
   };
--- server/interface/users.js
+++ server/interface/users.js
@@ -1,34 +1,26 @@
-import Router from 'koa-router'
+import Router from 'koa-router';
 import Redis from 'koa-redis'
 import nodeMailer from 'nodemailer'
-
 import User from '../dbs/models/user'
 import Passport from './utils/passport'
 import Email from '../dbs/config'
 import axios from './utils/axios'
 
-let router = new Router({
-  prefix: '/users'
-})
+let router = new Router({prefix: '/users'})
 
 let Store = new Redis().client
 
 router.post('/signup', async (ctx) => {
-  const {
-    username,
-    password,
-    email,
-    code
-  } = ctx.request.body;
+  const {username, password, email, code} = ctx.request.body;
 
-  if(code) {
-    const saveCode = await Store.hget(`nodemail:${username}`,'code')
+  if (code) {
+    const saveCode = await Store.hget(`nodemail:${username}`, 'code')
     const saveExpire = await Store.hget(`nodemail:${username}`, 'expire')
-    if(code === saveCode) {
-      if(new Date().getTime() - saveExpire > 0) {
+    if (code === saveCode) {
+      if (new Date().getTime() - saveExpire > 0) {
         ctx.body = {
-          code: -1, 
-          msg: '验证码已过期，请重新发送'
+          code: -1,
+          msg: '验证码已过期，请重新尝试'
         }
         return false
       }
@@ -44,27 +36,17 @@ router.post('/signup', async (ctx) => {
       msg: '请填写验证码'
     }
   }
-
-  let user = await User.find({
-    username
-  })
-  if(user.length) {
-    ctx.body= {
+  let user = await User.find({username})
+  if (user.length) {
+    ctx.body = {
       code: -1,
       msg: '已被注册'
     }
-    return 
+    return
   }
-  let nuser = await User.create({
-    username,
-    passsword,
-    email
-  })
-  if(nuser) {
-    let res = await axios.post('/users/signin', {
-      username,
-      passsword
-    })
+  let nuser = await User.create({username, password, email})
+  if (nuser) {
+    let res = await axios.post('/users/signin', {username, password})
     if (res.data && res.data.code === 0) {
       ctx.body = {
         code: 0,
@@ -76,7 +58,7 @@ router.post('/signup', async (ctx) => {
         code: -1,
         msg: 'error'
       }
-    } 
+    }
   } else {
     ctx.body = {
       code: -1,
@@ -86,17 +68,17 @@ router.post('/signup', async (ctx) => {
 })
 
 router.post('/signin', async (ctx, next) => {
-  return Passport.authenticate('local', function(err, user, info, status){
-    if(err) {
+  return Passport.authenticate('local', function(err, user, info, status) {
+    if (err) {
       ctx.body = {
         code: -1,
         msg: err
       }
     } else {
-      if(user) {
+      if (user) {
         ctx.body = {
           code: 0,
-          msg: '登陆成功',
+          msg: '登录成功',
           user
         }
         return ctx.login(user)
@@ -112,18 +94,15 @@ router.post('/signin', async (ctx, next) => {
 
 router.post('/verify', async (ctx, next) => {
   let username = ctx.request.body.username
-  const saveExpire = await Store.hget(`nodemail: ${username}`, 'expire')
-  if(saveExpire && new Date().getTime() - saveExpire < 0) {
+  const saveExpire = await Store.hget(`nodemail:${username}`, 'expire')
+  if (saveExpire && new Date().getTime() - saveExpire < 0) {
     ctx.body = {
       code: -1,
-      msg: '验证请求过于频繁！'
+      msg: '验证请求过于频繁，1分钟内1次'
     }
     return false
   }
   let transporter = nodeMailer.createTransport({
-    // host: Email.host,
-    // prot: 587,
-    // secure: false,
     service: 'qq',
     auth: {
       user: Email.smtp.user,
@@ -137,27 +116,27 @@ router.post('/verify', async (ctx, next) => {
     user: ctx.request.body.username
   }
   let mailOptions = {
-    from: `"认证邮件"<${Email.smtp.user}>`,
+    from: `"认证邮件" <${Email.smtp.user}>`,
     to: ko.email,
-    subject: `您申请的注册码`,
-    html: `您申请的验证码是${ko.code}`
+    subject: '您的验证码是',
+    html: `您的邮箱验证码是${ko.code}`
   }
   await transporter.sendMail(mailOptions, (error, info) => {
-    if(error) {
-      return console.log('err')
+    if (error) {
+      return console.log(error)
     } else {
       Store.hmset(`nodemail:${ko.user}`, 'code', ko.code, 'expire', ko.expire, 'email', ko.email)
     }
   })
   ctx.body = {
     code: 0,
-    msg: '验证码已发送，验证码有效期为一分钟！'
+    msg: '验证码已发送，可能会有延时，有效期1分钟'
   }
 })
 
 router.get('/exit', async (ctx, next) => {
   await ctx.logout()
-  if(!ctx.isAuthenticated()) {
+  if (!ctx.isAuthenticated()) {
     ctx.body = {
       code: 0
     }
@@ -168,17 +147,17 @@ router.get('/exit', async (ctx, next) => {
   }
 })
 
-router.get('/getUser', async(ctx) => {
-  if(ctx.isAuthenticated()) {
+router.get('/getUser', async (ctx) => {
+  if (ctx.isAuthenticated()) {
     const {username, email} = ctx.session.passport.user
-    ctx.body = {
-      user: username,
+    ctx.body={
+      user:username,
       email
     }
-  } else {
-    ctx.body = {
-      user: '',
-      email: ''
+  }else{
+    ctx.body={
+      user:'',
+      email:''
     }
   }
 })
