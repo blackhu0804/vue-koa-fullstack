adc94c91ec45d76ae324630ac8c5578412c4246d
blackhu <812510003@qq.com>
2019-01-24 19:52:45 +0800

    完成切换城市页面

--- pages/changeCity.vue
+++ pages/changeCity.vue
@@ -6,24 +6,38 @@
       </el-col>
     </el-row>
     <el-row>
-      <el-col :span="24"></el-col>
+      <el-col :span="24">
+        <hot />
+      </el-col>
     </el-row>
     <el-row>
-      <el-col :span="24"></el-col>
+      <el-col :span="24">
+        <category />
+      </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
 import iSelect from '@/components/changeCity/iselect.vue'
-
+import hot from '@/components/changeCity/hot.vue'
+import category from '@/components/changeCity/category.vue'
 export default {
   components: {
-    iSelect
+    iSelect,
+    hot,
+    category
   }
 }
 </script>
 
-<style>
-
+<style lang="scss">
+  .page-changeCity {
+    dd {
+      cursor: pointer;
+      &:hover {
+        color: #41B883;
+      }
+    }
+  }
 </style>
