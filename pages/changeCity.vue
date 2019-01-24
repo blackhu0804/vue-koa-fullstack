610ecb3d650398699a9db2f4d3dae72f434a376e
blackhu <812510003@qq.com>
2019-01-24 15:42:36 +0800

    修改为本地接口

---
+++ pages/changeCity.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="page-changeCity">
+    <el-row>
+      <el-col :span="24"></el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="24"></el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="24"></el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
